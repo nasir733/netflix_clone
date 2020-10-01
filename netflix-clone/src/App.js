5d65f76329e97a10b4ed71f37d9b124bd9a2e5a5
Nasir Iqbal <60297068+nasir733@users.noreply.github.com>
2020-10-02 00:25:37 +0500

    added tmdb Api

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import requests from "./requests";
+import Row from "./Row";
+
+function App() {
+  return (
+    <div className="App">
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="Aciton Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documantaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
